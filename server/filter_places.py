ac46744d3ae652ed192be413455c50e905326ed7
katanabana <katanabana01@gmail.com>
2024-08-27 23:38:27 +0500

    add distance calculation, fix css

--- server/filter_places.py
+++ server/filter_places.py
@@ -1,32 +1,24 @@
+import json
+
 from g4f.client import Client
 
 
 def get_places_ids(places, user_description):
-    context = ''
-    for place in places:
-        if 'name' not in place or 'categories' not in place:
-            continue
-        id = place['id']
-        name = place['name']
-        place_categories = place['categories']
-        associated_words_list = []
-        for category in place_categories:
-            associated_words_list.append(category)
-        associated_words = ', '.join(associated_words_list)
-        place_description = f'{id}. `{name}` - {associated_words}\n'
-        context += place_description
-    question = f'I have the following requirements for the route `{user_description}`\n'
-    question += 'Depending on the information about places, what route should I follow?\n'
+    context = json.dumps(places, ensure_ascii=False)
+    question = f'And requirements for the route: `{user_description}`\n'
+    question += 'Depending on the information about the places, what route should I follow?\n'
     question += 'Return list of ids of the places I should visit separated with commas, without any other text.'
 
-    content = context + question
+    content = 'I have the following info about places:\n```\nplaces = ' + context + '```\n' + question
 
+    print(content)
     client = Client()
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": content}],
     )
     ids_s = response.choices[0].message.content
+    print(ids_s)
     if ', ' in ids_s:
         ids_list = ids_s.split(', ')
     else:
@@ -39,4 +31,8 @@ def get_places_ids(places, user_description):
             if str(i) in ids_s:
                 ids_list.append(i)
                 ids_s = ids_s.replace(str(i), '')
+    if not ids_list:
+        for place in places:
+            if place['name'] in ids_s:
+                ids_list.append(place['id'])
     return ids_list
--- server/get_places.py
+++ server/get_places.py
@@ -1,6 +1,7 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from filter_places import get_places_ids
+import geopy.distance
 
 API_KEY = 'd548c5ed24604be6a9dd0d989631f783'
 
@@ -55,6 +56,9 @@ def get_places(lon, lat, user_description):
         for group in groups_of_places:
             for place in group:
                 if place not in places:
+                    dist = geopy.distance.geodesic((lat, lon), place['coordinates']).m
+                    place['distance_in_meters'] = int(dist)
+                    place.pop('coordinates')
                     places.append(place)
     for i, place in enumerate(places):
         place['id'] = i
@@ -64,6 +68,7 @@ def get_places(lon, lat, user_description):
         for place in places:
             if place['id'] in proper_ids:
                 proper_places.append(place)
+
     else:
         proper_places = places
     return proper_places

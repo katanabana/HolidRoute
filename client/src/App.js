e2fdd5494ea9ea15693b97ee983c0557ec16031d
katanabana <katanabana01@gmail.com>
2024-09-08 11:36:30 +0500

    - fix css and g4f prompt

--- client/src/App.js
+++ client/src/App.js
@@ -36,6 +36,13 @@ function App() {
     };
 
     wakeUpServer();
+
+    window.addEventListener("resize", () => {
+      const activeElement = document.activeElement;
+      if (activeElement.tagName === "textarea") {
+        activeElement.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+    });
   }, []);
 
   let footer = <div></div>;
--- server/filter_places.py
+++ server/filter_places.py
@@ -101,7 +101,7 @@ async def get_places_ids(places, user_description):
     question += 'to fit route requirements?\n'
     question += "If the route requirements don't say to go to multiple places, return a list of length 1.\n"
     question += "The output json dictionaries should include brief descriptions of the places to visit in the language "
-    question += "of the route requirements, not the language of the region where the places are located. "
+    question += "of the requirements for the route (not always english or the language of the region where the places are located). "
     question += "Each description should include the reason why you think the place fits the route requirements.\n"
     question += 'Return only a json list consisting of dictionaries in the following format:\n'
     question += '`[{"id": id, "name": name, "description": brief_description_of_the_place}, ...]`'

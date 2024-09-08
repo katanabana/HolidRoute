9858a38d19a0e64ad7a23637ef8ec7a75efea676
katanabana <katanabana01@gmail.com>
2024-09-08 12:36:42 +0500

    - remove keyboard open handler

--- client/src/MapWidget.js
+++ client/src/MapWidget.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import loaderIcon from "./icons/loader.png";
 import process from "process"; // Node.js process module imported
-import useKeyboardStatus from "./useKeyboardStatus";
 
 async function getPlaces(lon, lat, userDescription) {
   try {
@@ -19,7 +18,6 @@ const MapWidget = ({ showRoute, routeType, userDescription }) => {
   const [latitude, setLatitude] = useState(55.7558);
   const [longitude, setLongitude] = useState(37.6173);
   const [loading, setLoading] = useState(false);
-  const isKeyboardOpen = useKeyboardStatus();
 
   useEffect(() => {
     if (
@@ -141,12 +139,7 @@ const MapWidget = ({ showRoute, routeType, userDescription }) => {
 
   return (
     <>
-      <div
-        id="map"
-        className={
-          (loading ? " blur" : "") + (isKeyboardOpen ? " keyboard-open" : "")
-        }
-      >
+      <div id="map" className={loading ? " blur" : ""}>
         {" "}
         <img
           alt="img"
